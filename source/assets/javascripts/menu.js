f0534b5f1887dd171f1a9fe4d08776ab875d78b3
irinka <i.bakhareva@mail.ru>
2017-01-19 20:38:21 +0400

    Complete mobile and tablet

--- source/assets/javascripts/menu.js
+++ source/assets/javascripts/menu.js
@@ -6,4 +6,30 @@ $(function () {
       scrollTop: $(nameBlock).offset().top - 60
     }, 500);
   })
+
+  $('.menu-mobile_item > a').click(function (e) {
+    e.preventDefault();
+    var nameBlock = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(nameBlock).offset().top
+    }, 500);
+  })
+
+  function show() {
+    $('.menu-mobile_btn').unbind();
+    $('.menu-mobile_btn').click(function () {
+      $('.menu-mobile').fadeIn(200);
+      hide();
+    })
+  }
+
+  function hide() {
+    $('.menu-mobile_btn').unbind();
+    $('.menu-mobile_btn').click(function () {
+      $('.menu-mobile').fadeOut(200);
+      show();
+    })
+  }
+
+  hide();
 });
